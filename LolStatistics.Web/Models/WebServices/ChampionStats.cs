7767f1b39bbfede60a7610464b8395351bdca321
Kukukun <theophile.varnier@gmail.com>
2015-05-05 15:06:53 +0200

    ok on oublie

---
+++ LolStatistics.Web/Models/WebServices/ChampionStats.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace LolStatistics.Web.Models.WebServices
+{
+    [JsonObject]
+    public class ChampionStats
+    {
+        [JsonProperty("id")]
+        public int ChampionId { get; set; }
+
+        [JsonProperty("stats")]
+        public ChampionStatisticsViewModel Stats { get; set; }
+    }
+}
