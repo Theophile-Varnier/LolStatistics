15faf6272bb614ba04a7d818f73da148a0ac56ec
Kukukun <theophile.varnier@gmail.com>
2015-05-05 10:56:50 +0200

    team stalker suite et plus ou moins fin

--- LolStatistics.Web/Services/RankedStatsService.cs
+++ LolStatistics.Web/Services/RankedStatsService.cs
@@ -13,7 +13,28 @@ namespace LolStatistics.Web.Services
 {
     public class RankedStatsService
     {
+        /// <summary>
+        /// Récupère les statistiques de la saison d'un invocateur
+        /// </summary>
+        /// <param name="summonerId"></param>
+        /// <returns></returns>
         public TeamStalkerViewModel GetStatsForSummoner(long summonerId)
+        {
+            IList<Participant> participations = GetSummonerHistory(summonerId);
+
+            TeamStalkerViewModel res = StatisticsMapper.MapToTeamModel(participations);
+
+            res.Name = GetSummonerName(summonerId);
+
+            return res;
+        }
+
+        /// <summary>
+        /// Récupère l'ensemble des stats de la saison d'un invocateur
+        /// </summary>
+        /// <param name="summonerId">L'id de l'invocateur dont on veut récupérer les stats</param>
+        /// <returns></returns>
+        private IList<Participant> GetSummonerHistory(long summonerId)
         {
             List<Participant> participations = new List<Participant>();
             WebServiceConsumer<MatchHistory> rankedGameWebServiceConsumer = new WebServiceConsumer<MatchHistory>(ConfigurationManager.AppSettings["BaseUri"], ConfigurationManager.AppSettings["SummonerHistoryUrl"]);
@@ -22,12 +43,17 @@ namespace LolStatistics.Web.Services
             int beginIndex = 0;
             parameters.Add("beginIndex", beginIndex.ToString(CultureInfo.InvariantCulture));
             parameters.Add("endIndex", (beginIndex + 14).ToString(CultureInfo.InvariantCulture));
+
+            // Tant qu'il y a des parties, on les récupère
             while ((games = rankedGameWebServiceConsumer.Consume(parameters)).Matches != null)
             {
                 foreach (RankedGame game in games.Matches)
                 {
+                    // On récupère seulement les parties de la saison qui nous intéresse
+                    // TODO: Saison actuelle à paramétrer en base
                     if (game.Season == "SEASON2015")
                     {
+                        // On garde seulement notre invocateur
                         foreach (ParticipantIdentity pi in game.ParticipantIdentities)
                         {
                             if (pi.Player.Id != summonerId)
@@ -38,28 +64,35 @@ namespace LolStatistics.Web.Services
                         participations.AddRange(game.Participants);
                     }
                 }
+                // On passe à la page suivante
                 beginIndex += 15;
                 parameters["beginIndex"] = (beginIndex).ToString(CultureInfo.InvariantCulture);
                 parameters["endIndex"] = (beginIndex + 14).ToString(CultureInfo.InvariantCulture);
             }
+            return participations;
+        }
 
-            TeamStalkerViewModel res = StatisticsMapper.MapToTeamModel(participations);
-
+        /// <summary>
+        /// Récupère le nom d'un invocateur à l'aide de son id
+        /// Via le cache si on le connaît
+        /// Via un web service sinon
+        /// </summary>
+        /// <param name="summonerId"></param>
+        /// <returns></returns>
+        private string GetSummonerName(long summonerId)
+        {
             string summonerName = LolStatisticsCache.GetSummonerName(summonerId);
             if (summonerName != null)
             {
-                res.Name = summonerName;
-            }
-            else
-            {
-                WebServiceConsumer<Summoners> summonerWebServiceConsumer = new WebServiceConsumer<Summoners>(ConfigurationManager.AppSettings["BaseUri"], ConfigurationManager.AppSettings["SummonerByIdApi"]);
-                Dictionary<string, string> parametres = new Dictionary<string, string> { { "summonerIds", summonerId.ToString(CultureInfo.InvariantCulture) } };
-                Summoners current = summonerWebServiceConsumer.Consume(parametres);
-                res.Name = current.First().Value.Name;
-                LolStatisticsCache.AddSummonerName(summonerId, res.Name);
+                return summonerName;
             }
 
-            return res;
+            WebServiceConsumer<Summoners> summonerWebServiceConsumer = new WebServiceConsumer<Summoners>(ConfigurationManager.AppSettings["BaseUri"], ConfigurationManager.AppSettings["SummonerByIdApi"]);
+            Dictionary<string, string> parametres = new Dictionary<string, string> { { "summonerIds", summonerId.ToString(CultureInfo.InvariantCulture) } };
+            Summoners current = summonerWebServiceConsumer.Consume(parametres);
+            string name = current.First().Value.Name;
+            LolStatisticsCache.AddSummonerName(summonerId, name);
+            return name;
         }
     }
 }
